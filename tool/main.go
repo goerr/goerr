bb69b828ce963bcf2d357590f08322618ddbe572
goen <fuuucl.am@gmail.com>
2014-11-04 22:25:40 +0100

    A simple handling

--- tool/main.go
+++ tool/main.go
@@ -3,9 +3,84 @@ package main
 import (
 	"fmt"
 	"github.com/codegangsta/cli"
+	"os"
 )
 
+func hanAction(c *cli.Context) {
+	fmt.Println("added task han: ", c)
+}
+
 func main() {
+
+	// global level flags
+	flagz := []cli.Flag{
+		cli.BoolFlag{
+			Name:  "verbose",
+			Usage: "Show more output",
+		},
+		cli.StringFlag{
+			Name:  "f, file",
+			Usage: "Specify an alternate fig file (default: fig.yml)",
+		},
+		cli.StringFlag{
+			Name:  "p, project-name",
+			Usage: "Specify an alternate project name (default: directory name)",
+		},
+	}
+
+	// Commands
+	cmdz := []cli.Command{
+		{
+			Name: "han",
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "f",
+					Usage: "Foo.",
+				},
+			},
+			Usage:  "Handle errors in place",
+			Action: hanAction,
+		},
+		{
+			Name: "del",
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "f",
+					Usage: "Foo.",
+				},
+			},
+			Usage:  "Handle errors in errfile",
+			Action: hanAction,
+		},
+		{
+			Name: "init",
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "f",
+					Usage: "Foo.",
+				},
+			},
+			Usage:  "Create an empty errfile",
+			Action: hanAction,
+		},
+		{
+			Name: "ls",
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "f",
+					Usage: "Foo.",
+				},
+			},
+			Usage:  "List unhandled",
+			Action: hanAction,
+		},
+	}
+
+	app := cli.NewApp()
+	app.Flags = flagz
+	app.Commands = cmdz
+
 	fmt.Println("hello world")
 
+	app.Run(os.Args)
 }
