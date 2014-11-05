598252c122b11ec7d11e857ba63b7cab59a87b97
goen <fuuucl.am@gmail.com>
2014-11-05 11:36:36 +0100

    Add the interface cast to make it pass

--- goerr/main.go
+++ goerr/main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"github.com/codegangsta/cli"
 	"github.com/davecgh/go-spew/spew"
-	//	"go/ast"
+	"go/ast"
 	"go/parser"
 	"go/token"
 	//	"io/ioutil"
 	"os"
+	//	"strings"
 )
 
 func i() {
@@ -17,6 +18,84 @@ func i() {
 
 func massageAction(c *cli.Context) {
 	fmt.Println("massaging the codebase")
+}
+
+type myfileinfo struct {
+	is_goerr_import  bool
+	is_dotted_import bool
+}
+
+type spewlord struct{}
+
+func (spewlord) Visit(node ast.Node) ast.Visitor {
+
+	var fun *ast.SelectorExpr
+	var funx *ast.Ident
+	var funsel *ast.Ident
+
+	switch n := node.(type) {
+	case *ast.CallExpr:
+
+		switch fun := n.Fun.(type) {
+		case *ast.SelectorExpr:
+			/*
+				switch funsel := fun.Sel.(type) {
+				case *ast.Ident:
+			*/
+
+			switch funsel := interface{}(fun.Sel).(type) {
+			case *ast.Ident:
+
+				switch funx := fun.X.(type) {
+				case *ast.Ident:
+
+					_ = fun
+					_ = funx
+					_ = funsel
+
+					spew.Dump(n, fun, funx, funsel)
+
+					//				}
+				}
+			}
+		}
+
+	}
+
+	_ = fun
+	_ = funx
+	_ = funsel
+
+	/*
+		if funx.Name == "goerr" {
+			if funsel.Name == "XQZ" {
+				fmt.Println("found recover wrapper")
+			}
+			if funsel.Name[:2] == "OR" {
+				fmt.Println("found return wrapper")
+			}
+
+		}
+	*/
+
+	/*
+
+		m := n
+
+	*/
+
+	/*
+		var efns map[string]bool
+
+		efns["errB"] = true
+		efns["errA"] = true
+	*/
+	//	if efns[node.Ident] {
+	//		fmt.Println("found cool node:")
+
+	//	}
+
+	return spewlord{}
 }
 
 func hanAction(c *cli.Context) {
@@ -42,8 +121,31 @@ func hanAction(c *cli.Context) {
 	fc, _ := parser.ParseFile(fsetc, codefile, nil, 0)
 	fe, _ := parser.ParseFile(fsete, errfile, nil, 0)
 
-	spew.Dump(fc)
-	spew.Dump(fe)
+	var myc, mye myfileinfo
+
+	for _, s := range fc.Imports {
+		if s.Path.Value == "\"github.com/goerr/goerr\"" {
+			myc.is_goerr_import = true
+
+			if s.Name != nil && s.Name.Name == "." {
+				myc.is_dotted_import = true
+			}
+
+		}
+	}
+
+	_ = myc
+	_ = mye
+	_ = fc
+	_ = fe
+	for _, s := range fc.Decls {
+		//		spewlord{}.Visit(s)
+
+		ast.Walk(spewlord{}, s)
+	}
+	//	spew.Dump(fc.Imports)
+	//	spew.Dump(fc.Decls)
+	//	spew.Dump(fe.Decls)
 }
 
 func delAction(c *cli.Context) {
